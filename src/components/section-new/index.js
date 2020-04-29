9f543f4cc45584b6790663b7c49cde245aa85816
Agostina Caffaratti <agostinacaffaratti@gmail.com>
2020-04-29 13:15:30 -0300

    Estilos y componentes de la seccion noticias

--- src/components/section-new/index.js
+++ src/components/section-new/index.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 
 import New from '../articulo'
 import './style.scss'
+import MainSection from '../main-section'
+
 
 class News extends Component {
     render (){ 
+        
         return <section className = "news-container" >
+        <div className = 'main-section-container'>
+        <MainSection main = {this.props.news[0]}/>   
+        </div>
+        <div className = 'secondary-new'>
         {this.props.news.map( e => <New new= {e} key={e.id}/>)}
-        </section> }
+        </div>
+        </section> 
+        
+
+        }
 
 }
 
